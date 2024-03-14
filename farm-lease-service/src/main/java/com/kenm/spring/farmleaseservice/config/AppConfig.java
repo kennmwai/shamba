4fb4cc313e56e1635c196f79ec0f7f33f5dfdf37
FourtyThree43 <shaqmwa@outlook.com>
2024-03-15 01:27:44 +0300

    Uno commit

---
+++ farm-lease-service/src/main/java/com/kenm/spring/farmleaseservice/config/AppConfig.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.kenm.spring.farmleaseservice.config;
+
+/**
+ * @author User
+ *
+ */
+public class AppConfig {
+
+	/**
+	 * 
+	 */
+	public AppConfig() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ farm-lease-service/src/test/java/com/kenm/spring/farmleaseservice/FarmLeaseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kenm.spring.farmleaseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmLeaseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
