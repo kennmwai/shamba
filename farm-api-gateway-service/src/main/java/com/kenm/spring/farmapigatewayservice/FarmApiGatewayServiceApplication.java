185219e5e52ec381faf368c1113bf8bd23647dfc
FourtyThree43 <shaqmwa@outlook.com>
2024-03-20 15:46:12 +0300

    Added Servers stuff: see diff

--- farm-api-gateway-service/src/main/java/com/kenm/spring/farmapigatewayservice/FarmApiGatewayServiceApplication.java
+++ farm-api-gateway-service/src/main/java/com/kenm/spring/farmapigatewayservice/FarmApiGatewayServiceApplication.java
@@ -2,8 +2,10 @@ package com.kenm.spring.farmapigatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class FarmApiGatewayServiceApplication {
 
 	public static void main(String[] args) {
--- farm-config-server-service/src/main/java/com/kenm/spring/farmconfigserverservice/FarmConfigServerServiceApplication.java
+++ farm-config-server-service/src/main/java/com/kenm/spring/farmconfigserverservice/FarmConfigServerServiceApplication.java
@@ -2,8 +2,10 @@ package com.kenm.spring.farmconfigserverservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class FarmConfigServerServiceApplication {
 
 	public static void main(String[] args) {
