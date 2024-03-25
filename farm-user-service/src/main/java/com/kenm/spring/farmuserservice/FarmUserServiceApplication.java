955e3ee49270538dca6237074ed501f971d262c1
FourtyThree43 <shaqmwa@outlook.com>
2024-03-25 15:34:34 +0300

    API gateway, Eureka Discovery Server and User Service

---
+++ farm-user-service/src/main/java/com/kenm/spring/farmuserservice/FarmUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kenm.spring.farmuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmUserServiceApplication.class, args);
+	}
+
+}
