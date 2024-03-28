b310067ab2b68956a93f52664179685bf1341ece
FourtyThree43 <shaqmwa@outlook.com>
2024-03-28 23:30:17 +0300

    Client Service Init

---
+++ farm-client-service/src/main/java/com/kenm/spring/farmclientservice/controller/ClientController.java
@@ -0,0 +1,17 @@
+package com.kenm.spring.farmclientservice.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/client")
+public class ClientController {
+
+    @GetMapping
+    public ResponseEntity<String> home() {
+        return ResponseEntity.ok("Welcome to Shamba the Rent a Farm Service!");
+    }
+
+}
