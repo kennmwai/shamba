49828329f60fa0f37d9fc22c7e4087d933b82265
FourtyThree43 <shaqmwa@outlook.com>
2024-03-31 09:43:15 +0300

    Updated Controllers

--- farm-client-service/src/main/java/com/kenm/spring/farmclientservice/controller/ClientController.java
+++ farm-client-service/src/main/java/com/kenm/spring/farmclientservice/controller/ClientController.java
@@ -1,83 +1,17 @@
 package com.kenm.spring.farmclientservice.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.kenm.spring.farmclientservice.dto.UserDTO;
-import com.kenm.spring.farmclientservice.service.impl.UserServiceImpl;
-
-import jakarta.validation.Valid;
-
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping(path = "/api/v1", produces = "application/json")
 public class ClientController {
-
-    @Autowired
-    private UserServiceImpl userService;
 
     @RequestMapping
     public ResponseEntity<String> root() {
-        return ResponseEntity.status(HttpStatus.OK).body("Welcome to Shamba the Rent a Farm Service!");
-    }
-
-    @GetMapping("/users")
-    public ResponseEntity<List<UserDTO>> fetchAllUsers() {
-        List<UserDTO> users = userService.getAllUsers();
-        return ResponseEntity.status(HttpStatus.OK).body(users);
-    }
-
-    @GetMapping("/users/{id}")
-    public ResponseEntity<UserDTO> fetchUserById(@PathVariable Long id) {
-        UserDTO user = userService.getUserById(id);
-        return ResponseEntity.status(HttpStatus.OK).body(user);
-    }
-
-    @GetMapping("/users/username/{username}")
-    public ResponseEntity<UserDTO> fetchUserByUsername(@PathVariable String username) {
-        UserDTO user = userService.getUserByUsername(username);
-        return ResponseEntity.status(HttpStatus.OK).body(user);
-    }
-
-    @GetMapping("/users/email/{email}")
-    public ResponseEntity<UserDTO> fetchUserByEmail(@PathVariable String email) {
-        UserDTO user = userService.getUserByEmail(email);
-        return ResponseEntity.status(HttpStatus.OK).body(user);
-    }
-
-    @PostMapping("/users")
-    public ResponseEntity<UserDTO> createUser(@Valid @RequestBody UserDTO user) {
-        UserDTO createdUser = userService.createUser(user);
-        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
-    }
-
-
-    @PutMapping("/users/{id}")
-    public ResponseEntity<UserDTO> updateUser(@PathVariable Long id, @Valid @RequestBody UserDTO user) {
-        UserDTO updatedUser = userService.updateUser(id, user);
-        return ResponseEntity.status(HttpStatus.OK).body(updatedUser);
-    }
-
-    @GetMapping("/users/{id}/delete")
-    public ResponseEntity<String> deleteUser(@PathVariable Long id) {
-        userService.deleteUser(id);
-        return ResponseEntity.status(HttpStatus.OK).body("User deleted successfully");
-    }
-
-    @DeleteMapping("/users/{id}")
-    public ResponseEntity<String> softDeleteUser(@PathVariable Long id) {
-        userService.deleteUser(id);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("User deleted successfully");
+        return ResponseEntity.status(HttpStatus.MULTIPLE_CHOICES).body("This is the API root endpoint for the Farm Client Service.\nPlease refer to the API documentation for more information.");
     }
 
 }
