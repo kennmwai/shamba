e825a640cbb0fe8be6c49b4a99fc9fee1a7e0e7a
FourtyThree43 <shaqmwa@outlook.com>
2024-04-13 22:38:14 +0300

    Commons: Audit

---
+++ farm-commons/src/main/java/com/ken/spring/farmcommons/utils/AuditMetadata.java
@@ -0,0 +1,45 @@
+package com.ken.spring.farmcommons.utils;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(
+    value = {"created_at", "updated_at"},
+    allowGetters = true
+)
+@Data
+public abstract class AuditMetadata implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@CreatedDate
+	@Column(name = "CREATED_AT", updatable = false)
+	private LocalDateTime createdAt;
+
+	@CreatedBy
+	@Column(name = "CREATED_BY", updatable = false)
+	private String createdBy;
+
+    @LastModifiedDate
+    @Column(name = "UPDATED_AT")
+    private LocalDateTime lastModifiedAt;
+
+    @LastModifiedBy
+    @Column(name = "UPDATED_BY")
+    private String lastModifiedBy;
+}
---
+++ farm-commons/src/test/java/com/ken/spring/farmcommons/FarmCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ken.spring.farmcommons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
